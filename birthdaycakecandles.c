e1ee9b69e0d7d15bad8bfaaf4699e79efdaa3eaf
Zachary Bell <zacharybell@live.com>
2017-10-27 06:08:47 -0600

    finished birthday cake candle algorithm

---
+++ birthdaycakecandles.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int num_candles(int *arr, int arr_size) {
+	
+	int max = *arr;
+	int num_candles = 0;
+
+	for(int i=0; i<arr_size; i++) {
+		if(arr[i] > max) {
+			max = arr[i];
+			num_candles = 1;
+		}
+		else if(arr[i] == max)
+			num_candles++;
+	}
+
+	return num_candles;
+}
+
+int main() {
+	
+	int n;
+	scanf("%i", &n);
+	int *arr = malloc(sizeof(int) * n);
+	for(int i=0; i<n; i++) {
+		scanf("%i", &arr[i]);
+	}
+	int result = num_candles(arr, n);
+	printf("%d\n", result);
+
+	return 0;
+}
